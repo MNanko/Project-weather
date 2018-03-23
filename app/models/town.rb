0e4b6c8ea32b3076b3c16043dda995483100ab64
mohamed.nanko <mohamed.nanko@utbm.fr>
2018-03-23 16:53:26 -0400

    weather commit

--- app/models/town.rb
+++ app/models/town.rb
@@ -18,3 +18,4 @@ def forecast
     end
   end
 end
+
--- test/models/town_test.rb
+++ test/models/town_test.rb
@@ -4,4 +4,19 @@ class TownTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "town validation does geocoding" do
+    paris = Town.new
+    paris.town = "Paris"
+    paris.zipcode = 75000
+    assert paris.valid?
+    assert_equal(paris.lat, 48.8566101)
+    assert_equal(paris.lon, 2.3514992)
+  end
+  
+  test "town does not exist" do
+    unknown = Town.new
+    unknown.town = "MyString"
+    unknown.zipcode = 00000
+    assert !unknown.valid?
+  end
 end
